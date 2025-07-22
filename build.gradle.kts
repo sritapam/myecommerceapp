864069b00f30e9cb6b5e852023a188592974e219
sritapam <pereyrapame@gmail.com>
2025-07-21 21:31:10 -0300

    feat(repair): change version of "com.google.dagger.hilt.android"

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id ("org.jetbrains.kotlin.jvm") version "1.9.23" apply false
     alias(libs.plugins.android.library) apply false
+
 }
 buildscript {
     dependencies {
